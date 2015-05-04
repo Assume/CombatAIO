07b6b010d967d2938e8b2dfd87dff2f59e7fa462
Adam <one@pbot.org>
2015-05-03 17:53:17 -0700

    minor

--- com/base/api/general/walking/CWalking.java
+++ com/base/api/general/walking/CWalking.java
@@ -1,7 +1,7 @@
 package scripts.CombatAIO.com.base.api.general.walking;
 
-import org.tribot.api.interfaces.Positionable;
 import org.tribot.api2007.WebWalking;
+import org.tribot.api2007.types.RSTile;
 
 import scripts.CombatAIO.com.base.api.threading.Dispatcher;
 import scripts.CombatAIO.com.base.api.threading.types.ValueType;
@@ -10,12 +10,19 @@ import scripts.CombatAIO.com.base.api.types.enums.MovementType;
 public class CWalking {
 
 	public static void walk(MovementType type) {
+		RSTile end_tile = (type == MovementType.TO_BANK ? getNearestBank()
+				: (RSTile) Dispatcher.get().get(ValueType.HOME_TILE).getValue());
+		Teleporting.attemptToTeleport(end_tile);
 		if (type.equals(MovementType.TO_BANK))
 			WebWalking.walkToBank();
 		else
-			WebWalking.walkTo((Positionable) Dispatcher.get()
-					.get(ValueType.HOME_TILE).getValue());
+			WebWalking.walkTo(end_tile);
 		return;
 
+	}
+
+	private static RSTile getNearestBank() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
