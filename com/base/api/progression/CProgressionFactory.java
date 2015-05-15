2bbba5f2d2fe49b0f2986d734a8d3bfab3fb4bc7
Adam <one@pbot.org>
2015-05-14 21:52:35 -0700

    little more work

---
+++ com/base/api/progression/CProgressionFactory.java
@@ -0,0 +1,5 @@
+package scripts.CombatAIO.com.base.api.progression;
+
+public class CProgressionFactory {
+
+}
---
+++ com/base/api/progression/conditions/CConditions.java
@@ -0,0 +1,36 @@
+package scripts.CombatAIO.com.base.api.progression.conditions;
+
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+import org.tribot.api2007.Skills.SKILLS;
+
+import scripts.CombatAIO.com.base.api.progression.CProgressionMove;
+
+public enum CConditions {
+
+	TIME_ELAPSED {
+		@Override
+		CProgressionMove make() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	},
+	CURRENT_LEVEL {
+		@Override
+		CProgressionMove make() {
+			JComboBox<SKILLS> jcb = new JComboBox<SKILLS>(new SKILLS[] {
+					SKILLS.ATTACK, SKILLS.STRENGTH, SKILLS.HITPOINTS,
+					SKILLS.RANGED, SKILLS.HITPOINTS });
+			JOptionPane.showMessageDialog(null, jcb, "Select skill to track",
+					JOptionPane.QUESTION_MESSAGE, null);
+			SKILLS skill = (SKILLS) jcb.getSelectedItem();
+			int level = Integer.parseInt(JOptionPane.showInputDialog(
+					"Enter level: ").replaceAll("[^0-9]", ""));
+			return null;
+		}
+	};
+
+	abstract CProgressionMove make();
+
+}
